d887298ad02df10eff3f7c165370b17a20cbf3dd
Kimani-Dominic <muiruridominic001@gmail.com>
2024-03-18 23:40:09 +0300

    App.tsx updates

--- Jadili-react/src/App.tsx
+++ Jadili-react/src/App.tsx
@@ -1,22 +1,19 @@
-// Jadili-react/src/App.tsx
-import { useEffect, useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar'; // Import the Navbar component
+import Home from './components/Home'; //Home component
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
+
 
 function App() {
-  const [message, setMessage] = useState<string | null>(null);
-
-  useEffect(() => {
-    fetch('./api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
   return (
-    <div>
-      <h1>Jadili Blog-App</h1>
-      {message && <p>{message}</p>}
-    </div>
+    <Router>
+      <Navbar isAuthenticated={undefined} user={undefined} handleCategoryClick={undefined} handleLogout={undefined} />
+      <Home />
+    </Router>
+   
   );
 }
 
