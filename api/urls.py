ea1e1fb3d4c2b3d63b97d06971895d281e9792bc
Kimani-Dominic <muiruridominic001@gmail.com>
2025-02-01 10:36:25 +0300

    fix(app): User Registration

--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,13 @@
-#urls.py
-from django.urls import path, re_path
-from django.views.generic import TemplateView
-from .views import UserRegistrationView, UserLoginView, BlogPostListView, AddCommentView
-# from rest_framework import routers, views
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+from rest_framework.routers import DefaultRouter
 
-
-# router = routers.DefaultRouter()
-# router.register(r'Post',views., 'comment')
-
+router = DefaultRouter()
+router.register(r'profile', ProfileViewset)
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('login/', UserLoginView.as_view(), name='user-login'),
-    #Default Landing Page url
-    path('', BlogPostListView.as_view(), name='blog-post-list'),  
-    path('posts/<int:post_id>/comment/', AddCommentView.as_view(), name='add-comment'),
-    # Add other URLs as needed
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),  # Serve React app for all other URLs
-
+    path('api/', include(router.urls)),
+    path('api/register/', UserRegistrationView.as_view(), name='register'),
+    path('api/login/', UserLoginView.as_view(), name='login'),
 ]
