cf61a114fe6cd16a98fd846c9cad541004f28a1a
Kimani-Dominic <muiruridominic001@gmail.com>
2025-02-10 23:58:51 +0300

    fix(blog): comment

--- blog/models.py
+++ blog/models.py
@@ -12,14 +12,12 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-    #post category
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
     
-#comment
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -28,6 +26,6 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.created_at}"
+        return f"{self.user.AUTHOR} - {self.created_at}"
     
     
