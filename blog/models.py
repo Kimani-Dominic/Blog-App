2dfb0c3bcc0f15aa6079e645079fc3108376f333
Kimani-Dominic <muiruridominic001@gmail.com>
2025-02-11 19:01:52 +0300

    fix(app): models

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,17 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='api_posts'
+    )
+    categories = models.ManyToManyField(
+        Category, 
+        related_name='api_posts' 
+    )
 
     def __str__(self):
         return self.title
@@ -20,12 +29,17 @@ class Category(models.Model):
     
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='api_comments'
+    )
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user.AUTHOR} - {self.created_at}"
+        return f"{self.user} - {self.created_at}"
     
     
